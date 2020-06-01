3aacd3cfc435e63ae75d4f1eb4451b529a326faa
mrjoe3012 <51813263+mrjoe3012@users.noreply.github.com>
2020-06-01 16:38:40 +0100

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,5 +1,6 @@
 shell.setAlias("echo", "/programs/echo")
 shell.setAlias("redirect", "/programs/redirect")
 shell.setAlias("pipe", "/programs/pipe")
+shell.setAlias("find", "/programs/find")
 
 os.loadAPI("/lib/utils.lua")
